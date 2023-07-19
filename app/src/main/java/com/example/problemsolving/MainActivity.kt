08981667d1e20612aa923c452f73293fe00b4f1d
basemomar94 <basemomar2012@gmail.com>
2023-07-19 18:29:03 +0300

    hacker rank

--- app/src/main/java/com/example/problemsolving/MainActivity.kt
+++ app/src/main/java/com/example/problemsolving/MainActivity.kt
@@ -3,6 +3,7 @@ package com.example.problemsolving
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import kotlin.math.abs
 
 class MainActivity : AppCompatActivity() {
     private val TAG = "google"
@@ -10,11 +11,91 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val list = intArrayOf(7, 1, 5, 3, 6, 4)
-        val target = 5
+        val test = arrayOf(3, 10, 2, 9)
+        bonAppetit(test, 1, 12)
+    }
 
-        maxProfit(list)
+    fun bonAppetit(bill: Array<Int>, k: Int, b: Int): Unit {
+        // Write your code here
+        val annaBill = bill
+        annaBill.toMutableList().removeAt(k)
+        val annMoney = annaBill.sum() / 2
+        if (annMoney == b) {
+            println("Bon Appetit")
+        } else {
+            println(b-annMoney)
+        }
 
+    }
+
+    fun breakingRecords(scores: Array<Int>): Array<Int> {
+        // Write your code here
+        var minScore = scores.first()
+        var maxScore = scores.first()
+        var minScoreCount = 0
+        var maxScoreCount = 0
+        scores.forEach { score ->
+            when {
+                score > maxScore -> {
+                    Log.d(TAG, "current $score >  $maxScore")
+                    maxScore = score
+                    maxScoreCount += 1
+                }
+                score < minScore -> {
+                    minScore = score
+                    minScoreCount += 1
+                }
+            }
+
+        }
+        Log.d(TAG, "hack" + listOf(minScoreCount, maxScoreCount).toString())
+        return arrayOf(maxScoreCount, minScoreCount)
+    }
+
+    fun miniMaxSum(arr: Array<Int>): Unit {
+        val sortedList = arr.sorted()
+        val min = sortedList.take(4).sumOf { it.toLong() }
+        val max = sortedList.takeLast(4).sumOf { it.toLong() }
+        Log.d(TAG, "$min $max")
+        println("$min  $max")
+    }
+
+    fun birthdayCakeCandles(candles: Array<Int>): Int {
+        var tallestCandle: Int = 0
+        candles.forEach { candle ->
+            if (candle > tallestCandle) {
+                tallestCandle = candle
+            }
+
+        }
+        return candles.filter { it == tallestCandle }.size
+
+    }
+
+    fun staircase(n: Int): Unit {
+        for (i in 1..n) {
+            val space = StringBuilder()
+            val stair = StringBuilder()
+            space.append(" ".repeat(n - i))
+            stair.append("#".repeat(i))
+            println(space.toString() + stair)
+        }
+    }
+
+
+    fun diagonalDifference(arr: List<List<Int>>): Int {
+        // Write your code here
+        val leftMatrix = mutableListOf<Int>()
+        val rightMatrix = mutableListOf<Int>()
+        for (index in arr.indices) {
+            val rowNum = arr.size - 1
+            rightMatrix.add(arr[index][index])
+            leftMatrix.add(arr[index][rowNum - index])
+        }
+        Log.d(TAG, "left $leftMatrix")
+        Log.d(TAG, "right $rightMatrix")
+        val differentSum = abs(leftMatrix.sum() - rightMatrix.sum())
+        return differentSum
 
     }
 
@@ -38,7 +119,7 @@ class MainActivity : AppCompatActivity() {
             }
 
         }
-       Log.d(TAG, "profit list is $profitList  max profit ")
+        Log.d(TAG, "profit list is $profitList  max profit ")
         return profitList.maxOrNull()
 
     }
